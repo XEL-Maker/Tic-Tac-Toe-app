29f4341e48035ba7bc090542e1770e20ff1ede3d
Hyunho Kim <void_laboratory@naver.com>
2021-05-09 04:14:51 +0900

    승패판정 성송

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,12 +1,14 @@
-1package com.example.tictactoe;
+package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.tictactoe.R;
 
@@ -22,7 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     //p2 => 1
     //공백 => 2
     int[]gameState={2,2,2,2,2,2,2,2,2};
-    int[][]winingPosition=
+    int[][]winingPositions=
             {{0,1,2},{3,4,5},{6,7,8},//가로
                     {0,3,6},{1,4,7},{2,5,8},//세로
                     {0,4,8},{2,4,6}//대각선
@@ -43,9 +45,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             buttons[i]=(Button)findViewById(resId);
             buttons[i].setOnClickListener(this);
         }
+
+        roundCount=0;
+        playerOneScoreCount=0;
+        playerTwoScoreCount=0;
+        activePlayer=true;
     }
 
     @Override
     public void onClick(View v) {
+        if(!((Button)v).getText().toString().equals("")){
+            return;
+        }
+        String btnId=v.getResources().getResourceEntryName(v.getId());
+        int gameStatePointer= Integer.parseInt(btnId.substring(btnId.length()-1,btnId.length()));
+        if(activePlayer){
+            ((Button)v).setText("X");
+            ((Button)v).setTextColor(Color.parseColor("#FFC34A"));
+            gameState[gameStatePointer]=0;
+        }else{
+            ((Button)v).setText("O");
+            ((Button)v).setTextColor(Color.parseColor("#70FFEA"));
+            gameState[gameStatePointer]=1;
+        }
+        roundCount++;
+        if(checkWinner()){
+            if(activePlayer){
+                playerOneScoreCount++;
+                updatePlayerScore();
+                Toast.makeText(this,"Player One Won!", Toast.LENGTH_SHORT).show();
+                playAgain();
+            }else{
+                playerTwoScoreCount++;
+                updatePlayerScore();
+                Toast.makeText(this,"Player Two Won!", Toast.LENGTH_SHORT).show();
+                playAgain();
+            }
+        }else if(roundCount==9){
+            Toast.makeText(this,"No Winner!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            activePlayer=!activePlayer;
+        }
+    }
+    public boolean checkWinner(){
+        boolean winnerResult = false;
+        for(int [] winningPos : winingPositions){
+            if(gameState[winningPos[0]]==gameState[winningPos[1]]&&
+                    gameState[winningPos[1]]==gameState[winningPos[2]]&&
+                    gameState[winningPos[0]]!=2){
+                winnerResult=true;
+            }
+        }
+        return winnerResult;
+    }
+    public  void updatePlayerScore(){
+        playerOneScore.setText(Integer.toString(playerOneScoreCount));
+        playerTwoScore.setText(Integer.toString(playerTwoScoreCount));
+    }
+
+    public void playAgain(){
+        roundCount=0;
+        activePlayer=true;
+        for(int i=0;i<buttons.length;i++){
+            gameState[i]=2;
+            buttons[i].setText("");
+        }
     }
 }
